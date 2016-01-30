d3236213922357e731334c4f2d53122e0283bd8c
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2016-01-30 15:09:30 +0000

    minor

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require(‘express’);
 var app = express();
-app.set(‘port’, process.env.PORT || 8081);
+app.set(‘port’, process.env.PORT || 1234);
 app.listen(app.get(‘port’));
 console.log('hello world');
