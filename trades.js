cd5e7574bb8350ac9ccf394b432fba9ee26dffab
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2016-01-31 17:06:56 +0000

    getting ready for aws codedeploy integration

---
+++ trades.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var Pusher = require('pusher-client');
+
+var pusher = new Pusher('de504dc5763aeef9ff52');
+var trades_channel = pusher.subscribe('live_trades');
+
+var server = http.createServer(
+  function(request, response) {
+    response.writeHead( 200, {"content-type": "text/plain"} );
+    response.write("Trades are written to the console...\n");
+    response.end();
+  }
+);
+
+server.listen( 8080 );
+
+trades_channel.bind('trade', function(data) {
+  console.log(data);
+});
