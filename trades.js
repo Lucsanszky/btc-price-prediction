748d8d91c2243aaab8a49535b1c8ec03a54db8fc
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2016-02-01 15:47:17 +0000

    added sample code to test db connection

--- trades.js
+++ trades.js
@@ -1,8 +1,54 @@
-var http = require('http');
-var Pusher = require('pusher-client');
+var http      = require('http');
+var Pusher    = require('pusher-client');
+var mongoose  = require('mongoose');
+var express   = require('express');
 
-var pusher = new Pusher('de504dc5763aeef9ff52');
+var pusher         = new Pusher('de504dc5763aeef9ff52');
 var trades_channel = pusher.subscribe('live_trades');
+var app            = express();
+
+var config = {
+      "USER"     : "",                  // if your database has user/pwd defined
+      "PASS"     : "",
+      "HOST"     : "ip-10-0-2-63.ec2.internal",  // the domain name of our MongoDB EC2 instance
+      "PORT"     : "27017",             // this is the default port mongoDB is listening for incoming queries
+      "DATABASE" : "test"         // the name of your database on that instance
+    };
+
+var dbPath  = "mongodb://" + config.USER + ":" +
+    config.PASS + "@"+
+    config.HOST + ":"+
+    config.PORT + "/"+
+    config.DATABASE;
+
+var standardGreeting = 'Hello World!';
+
+var db;              // our MongoDb database
+
+var greetingSchema;  // our mongoose Schema
+var Greeting;        // our mongoose Model
+
+// create our schema
+greetingSchema = mongoose.Schema({
+  sentence: String
+});
+// create our model using this schema
+Greeting = mongoose.model('Greeting', greetingSchema);
+
+// ------------------------------------------------------------------------
+// Connect to our Mongo Database hosted on another server
+//
+console.log('\nattempting to connect to remote MongoDB instance on another EC2 server '+config.HOST);
+
+if ( !(db = mongoose.connect(dbPath)) )
+  console.log('Unable to connect to MongoDB at '+dbPath);
+else 
+  console.log('connecting to MongoDB at '+dbPath);
+
+// connection failed event handler
+mongoose.connection.on('error', function(err){
+  console.log('database connect error '+err);
+}); // mongoose.connection.on()
 
 var server = http.createServer(
   function(request, response) {
