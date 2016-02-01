f8e62e1748900d940bcb53da4fa9d11da0a6f5e4
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2016-02-01 18:11:18 +0000

    added dynamodb configuration

--- trades.js
+++ trades.js
@@ -6,6 +6,37 @@ var AWS       = require("aws-sdk");
 var pusher         = new Pusher('de504dc5763aeef9ff52');
 var trades_channel = pusher.subscribe('live_trades');
 
+AWS.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName : "Trades",
+    KeySchema: [       
+        { AttributeName: "TradeID", KeyType: "HASH"},  //Partition key
+        { AttributeName: "Date", KeyType: "RANGE" }  //Sort key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "TradeID", AttributeType: "N" },
+        { AttributeName: "Date", AttributeType: "N" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 5, 
+        WriteCapacityUnits: 5
+    }
+};
+
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
+
 var server = http.createServer(
   function(request, response) {
     response.writeHead( 200, {"content-type": "text/plain"} );
