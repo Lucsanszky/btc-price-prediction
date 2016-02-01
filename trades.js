606d19469035a979166cd6afd25d30de60699c12
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2016-02-01 18:22:50 +0000

    minor

--- trades.js
+++ trades.js
@@ -7,6 +7,7 @@ var pusher         = new Pusher('de504dc5763aeef9ff52');
 var trades_channel = pusher.subscribe('live_trades');
 
 AWS.config.update({
+  region: "us-west-2",
   endpoint: "https://dynamodb.us-west-2.amazonaws.com"
 });
 
