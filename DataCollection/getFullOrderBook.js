b5a4f30aa174b1697580f5b095aa38a5c6703197
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2016-04-12 20:34:35 +0100

    added scan to retrieve the total count of order book items

--- DataCollection/getFullOrderBook.js
+++ DataCollection/getFullOrderBook.js
@@ -5,12 +5,48 @@ AWS.config.loadFromPath('/home/ec2-user/.ec2/credentials.json');
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 var params = {
-    TableName: "OrderBook"
+    TableName: 'OrderBook',
+    Count: 'true'
 };
 
-docClient.scan(params, function(err, data) {
-    if (err)
-        console.log(JSON.stringify(err, null, 2));
-    else
-        console.log(JSON.stringify(data, null, 2));
-});
+var scanComplete = false,
+    itemCountTotal = 0,
+    consumedCapacityUnitsTotal = 0;
+
+async.until( function() { return scanComplete; },
+             function (callback) {
+                docClient.scan(params, function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(result);
+                        
+                        if (typeof (result.LastEvaluatedKey) === 'undefined' ) {
+                            scanComplete = true;
+                        } else {
+                            params.ExclusiveStartKey = result.LastEvaluatedKey;
+                        }
+
+                        itemCountTotal += result.Count;
+                        consumedCapacityUnitsTotal += result.ConsumedCapacityUnits;
+
+                        if (!scanComplete) {
+                            console.log("cumulative itemCount " + itemCountTotal);
+                            console.log("cumulative capacity units " + consumedCapacityUnitsTotal);
+                        }
+                    }
+                    callback(err);
+                });
+             },
+
+             function (err) {
+                if (err) {
+                    console.log('error in processing scan ');
+                    console.log(err);
+                } else {
+                    console.log('scan complete')
+                    console.log('Total items: ' + itemCountTotal);
+                    console.log('Total capacity units consumed: ' + consumedCapacityUnitsTotal);
+                }
+             }
+);
