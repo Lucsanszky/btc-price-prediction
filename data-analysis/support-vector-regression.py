398afec2afe5e85a2ef5a1e4ca71b1bc7be03352
Daniel Lucsanszky <dl3913@ubu30gb-templ.doc.ic.ac.uk>
2016-05-18 04:00:05 +0100

    cleaned up brr, added more datasets with different timesteps, added baseline to SGD/SVR evaluation

--- data-analysis/support-vector-regression.py
+++ data-analysis/support-vector-regression.py
@@ -8,7 +8,7 @@
 # * http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=1257413&tag=1
 # * http://www.sciencedirect.com/science/article/pii/S0305048301000263
 
-# In[1]:
+# In[29]:
 
 get_ipython().magic('matplotlib inline')
 
@@ -19,7 +19,7 @@ get_ipython().magic('load_ext version_information')
 get_ipython().magic('version_information deap, matplotlib, numpy, pandas, pymc3, seaborn, sklearn, theano')
 
 
-# In[2]:
+# In[30]:
 
 from nsga2 import *
 from IPython.display import display
@@ -33,6 +33,7 @@ import seaborn as sns
 from sklearn import linear_model
 from sklearn import preprocessing as preproc
 from sklearn import svm
+from sklearn.dummy import DummyRegressor
 from sklearn.metrics import mean_squared_error as mse, accuracy_score as acc_scr, mean_absolute_error as mae
 import theano
 from theano import tensor as T
@@ -42,13 +43,33 @@ np.set_printoptions(threshold=np.nan)
 sns.set()
 
 
-# In[3]:
+# In[31]:
+
+def directional_symmetry(act, pred):
+    act_ticks = list(map(lambda t: 1 if t[1] - t[0] >= 0 else 0, zip(y_test.values, y_test.values[1:])))
+    pred_ticks = list(map(lambda t: 1 if t[1] - t[0] >= 0 else 0, zip(pred, pred[1:])))
+    d = list(map(lambda t: t[0] == t[1], zip(act_ticks, pred_ticks)))
+    
+    return np.sum(d) / len(act_ticks)
+
+
+# In[46]:
 
 path = '../btc-data/BTC_LOB_features_10s.csv'
 data = pd.read_csv(path, index_col = 0, parse_dates = True)
 
 
-# In[4]:
+# In[ ]:
+
+data
+
+
+# In[47]:
+
+data = data.reindex(pd.date_range(start = data.index[0], end = data.index[-1], freq='10s'))
+
+
+# In[48]:
 
 X, y = data, data['mid price'].copy()
 
@@ -60,7 +81,7 @@ valid_dates = X.index[int(0.7*len(X)):int(0.85*len(X))]
 test_dates = X.index[int(0.85*len(X)):]
 
 
-# In[5]:
+# In[49]:
 
 X_train = pd.DataFrame()
 y_train = y[train_dates[2] : train_dates[-1]]
@@ -75,7 +96,7 @@ for c in ['ask price', 'bid price', 'mean ask price', 'mean bid price']:
 X_train.set_index(train_dates[2:], inplace = True)
 
 
-# In[6]:
+# In[50]:
 
 X_valid = pd.DataFrame()
 y_valid = y[valid_dates[2] : valid_dates[-1]]
@@ -90,7 +111,7 @@ for c in ['ask price', 'bid price', 'mean ask price', 'mean bid price']:
 X_valid.set_index(valid_dates[2:], inplace = True)
 
 
-# In[7]:
+# In[51]:
 
 X_test = pd.DataFrame()
 y_test = y[test_dates[2] : test_dates[-1]]
@@ -105,7 +126,7 @@ for c in ['ask price', 'bid price', 'mean ask price', 'mean bid price']:
 X_test.set_index(test_dates[2:], inplace = True)
 
 
-# In[85]:
+# In[52]:
 
 #def filter_features(mask):
 #    return list(map(lambda t: t[1], filter(lambda t: t[0], zip(mask, FEATURES))))
@@ -120,30 +141,28 @@ def fitness_fun(model):
         indiv[0] = 0.002
     
     method.eta0 = indiv[0]
-    method.fit(X_train[int(0.25*len(X_train)):], y_train[int(0.25*len(y_train)):])
+    method.power_t = indiv[1]
+    method.fit(X_train, y_train)
     
     X_valid25 = X_valid[:int(0.2*len(X_valid))]
     y_valid25 = y_valid[:int(0.2*len(y_valid))]
     
-    pred = method.predict(X_valid25)
-    rmse = np.sqrt(mse(y_valid25, pred))
-    
-    act_ticks = list(map(lambda t: 1 if t[1] - t[0] >= 0 else -1, zip(y_valid25.values, y_valid25.values[1:])))
-    pred_ticks = list(map(lambda t: 1 if t[1] - t[0] >= 0 else -1, zip(pred, pred[1:])))
-    act_pred_cmp = list(map(lambda t: t[0] == t[1], zip(act_ticks, pred_ticks)))
-    dir_sym = np.sum(act_pred_cmp) / len(act_ticks)
+    pred = method.predict(X_valid)
+    rmse = np.sqrt(mse(y_valid, pred))
+    dir_sym = directional_symmetry(y_valid, pred)
     
     return dir_sym, rmse
 
 def nsga2_feat_sel(method, gen_num, indiv_num):
-    creator.create("FitnessMulti", base.Fitness, weights = (1.0, -1.0))
+    creator.create("FitnessMulti", base.Fitness, weights = (1.0, -2.0))
     creator.create("Individual", list, fitness=creator.FitnessMulti) 
-    toolb.register('eta0', random.uniform, 0.002, 0.01)
-    toolb.register('individual', tools.initRepeat, creator.Individual, toolb.eta0, n = 1)
+    toolb.register('eta0', random.uniform, 0.0001, 0.01)
+    toolb.register('power_t', random.uniform, 0.001, 0.5)
+    toolb.register('individual', tools.initCycle, creator.Individual, (toolb.eta0, toolb.power_t), n = 1)
     toolb.register('population', tools.initRepeat, list, toolb.individual, n = indiv_num)
     toolb.register('evaluate', fitness_fun)
     toolb.register('mate', tools.cxUniform, indpb = 0.1)
-    toolb.register('mutate', tools.mutGaussian, mu = 0.002, sigma = 0.001, indpb = 0.1)
+    toolb.register('mutate', tools.mutGaussian, mu = 0.001, sigma = 0.01, indpb = 0.1)
     toolb.register('select', tools.selNSGA2)
 
     population = toolb.population()
@@ -177,12 +196,12 @@ def nsga2_feat_sel(method, gen_num, indiv_num):
     return best, chromosome
 
 
-# In[87]:
+# In[53]:
 
 def feature_selection(gen_num, indiv_num):
     
-    sgd = linear_model.SGDRegressor(loss='epsilon_insensitive', penalty='none',
-                                    learning_rate = 'constant', epsilon = 0.0, shuffle = False)
+    sgd = linear_model.SGDRegressor(loss='epsilon_insensitive', penalty='l2',
+                                    epsilon = 0.0)
 
     results = nsga2_feat_sel(sgd, gen_num, indiv_num)
 
@@ -199,23 +218,25 @@ def feature_selection(gen_num, indiv_num):
     
 widgets.interact(feature_selection,  
                  gen_num = 30, 
-                 indiv_num = 15)
+                 indiv_num = 10)
 
 
 # In[ ]:
 
-clf = svm.LinearSVR(C = 500, loss='epsilon_insensitive', max_iter = 50)
+clf = svm.LinearSVR(C = 5000, loss='epsilon_insensitive', max_iter = 100)
+clf.fit(X_train, y_train)
+
+
+# In[ ]:
+
+clf = svm.SVR(C = 50, kernel = 'rbf', max_iter = 10, cache_size = 10000)
 clf.fit(X_train, y_train)
 
 
 # In[ ]:
 
 pred = clf.predict(X_test)
-act_ticks = list(map(lambda t: 1 if t[1] - t[0] >= 0 else -1, zip(y_test.values, y_test.values[1:])))
-pred_ticks = list(map(lambda t: 1 if t[1] - t[0] >= 0 else -1, zip(pred, pred[1:])))
-act_pred_cmp = list(map(lambda t: t[0] == t[1], zip(act_ticks, pred_ticks)))
-accuracy = np.sum(act_pred_cmp) / len(act_ticks)
-accuracy
+di
 
 
 # In[ ]:
@@ -229,24 +250,21 @@ print(rmse_test)
 print(rmse_train)
 
 
-# In[81]:
+# In[54]:
 
-clf = linear_model.SGDRegressor(loss='epsilon_insensitive', penalty='none',
-                                epsilon=0, learning_rate = 'constant', shuffle = False, eta0 = 0.011268947838531019)
+clf = linear_model.SGDRegressor(loss='epsilon_insensitive', penalty='l2',
+                                epsilon=0, shuffle = True,
+                                eta0=0.002, power_t = 0.0759965388605597)
 clf.fit(X_train, y_train)
 
 
-# In[82]:
+# In[55]:
 
 pred = clf.predict(X_test)
-act_ticks = list(map(lambda t: 1 if t[1] - t[0] >= 0 else -1, zip(y_test.values, y_test.values[1:])))
-pred_ticks = list(map(lambda t: 1 if t[1] - t[0] >= 0 else -1, zip(pred, pred[1:])))
-act_pred_cmp = list(map(lambda t: t[0] == t[1], zip(act_ticks, pred_ticks)))
-accuracy = np.sum(act_pred_cmp) / len(act_ticks)
-accuracy
+directional_symmetry(y_test, pred)
 
 
-# In[83]:
+# In[56]:
 
 R2 = clf.score(X_test, y_test)
 R2
@@ -257,7 +275,7 @@ print(rmse_test)
 print(rmse_train)
 
 
-# In[84]:
+# In[43]:
 
 plt.figure(figsize = (20,10))
 plt.plot(y_test.index, y_test, label = 'Actual Prices')
@@ -265,6 +283,36 @@ plt.plot(y_test.index, pred, label = 'Predicted Prices')
 #plt.xlim('2016-04-24 19', '2016-04-24 21')
 #plt.ylim(445, 480)
 plt.legend()
+
+
+# In[57]:
+
+dr = DummyRegressor(strategy='mean')
+dr.fit(X_train, y_train)
+
+pred = dr.predict(X_test)
+print(directional_symmetry(y_test, pred))
+
+rmse_test = np.sqrt(mse(y_test, pred))
+rmse_train = np.sqrt(mse(y_train, clf.predict(X_train)))
+
+print(rmse_test)
+print(rmse_train)
+
+
+# In[58]:
+
+plt.figure(figsize = (20,10))
+plt.plot(y_test.index, y_test, label = 'Actual Prices')
+plt.plot(y_test.index, pred, label = 'Predicted Prices')
+#plt.xlim('2016-04-24 19', '2016-04-24 21')
+#plt.ylim(445, 480)
+plt.legend()
+
+
+# In[59]:
+
+np.mean(list(map(lambda t: np.abs(t[1] - t[0]), zip(y_test.values, y_test.values[1:]))))
 
 
 # In[ ]:
